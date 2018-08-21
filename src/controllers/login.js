4ff7a203d38da201173eab1e98170e5528d24cb2
onHawk <luisd824@gmail.com>
2018-08-21 19:53:04 -0400

    login gives back full user

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,37 +1,22 @@
-const jwt = require('jsonwebtoken');
-
 const User = require('../users/User.js');
 const { secret } = require('../config.js');
+const { makeToken } = require('../authenticate');
 
 const login = (req, res) => {
-  const { username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      res.status(403).json({ error: 'Invalid Username/Password' });
-      return;
-    }
-    if (user === null) {
-      res.status(422).json({ error: 'No user with that username in our DB' });
-      return;
-    }
-    user.checkPassword(password, (nonMatch, hashMatch) => {
-      // This is an example of using our User.method from our model.
-      if (nonMatch !== null) {
-        res.status(422).json({ error: 'passwords dont match' });
-        return;
-      }
-      if (hashMatch) {
-        const payload = {
-          username: user.username
-        };
-        const token = jwt.sign(payload, secret);
-        console.log(username);
-        res.json({ token });
-      }
+  const { _id } = req.user;
+  // console.log(req.user);
+  const token = makeToken(req.user);
+  User.findOne(_id)
+    .populate('notes')
+    .then(user => {
+      // console.log(user);
+      res.json({ token, user });
+    })
+    .catch(err => {
+      res.send(err);
     });
-  });
 };
 
 module.exports = {
-  login
+  login,
 };
